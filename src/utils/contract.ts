3659faa56d2e5bdd32291f226a8208995fbd17a3
Rares <raresgeo9@gmail.com>
2023-07-11 17:19:15 +0300

    fix: throw error when contracts folder is missing on deploy

--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -4,6 +4,11 @@ import fs from 'fs';
 export function getRootContractName(projectPath: string): string {
   // Find the first .sol file
   const contractsPath = path.join(projectPath, 'contracts');
+  if (!fs.existsSync(contractsPath)) {
+    throw new Error(
+      'No contracts folder found. Please run this command in the root of your project.'
+    );
+  }
   const files = fs.readdirSync(contractsPath);
   const solFiles = files.filter((file: string) => {
     return file.endsWith('.sol');
