fd757dc074b1e5aa1be89226d7bf79024438f4e9
Daniel PC <raresgeo9@gmail.com>
2023-10-31 10:06:30 +0200

    fix: single file contracts

--- src/tests/path.test.ts
+++ src/tests/path.test.ts
@@ -7,68 +7,88 @@ import {
 import * as path from "path";
 import fs from "fs/promises"; // Import the promise-based version
 
+interface Contract {
+  chainId: string;
+  contractAddress: string;
+}
+
 describe("Cloning", () => {
   let tempFolder: string;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     tempFolder = path.join(__dirname, "temp");
-  });
-
-  afterAll(async () => {
     await fs.rm(tempFolder, { recursive: true, force: true });
   });
 
-  it("create proper project structure", async () => {
-    const contracts = [
-      {
-        chainId: "1",
-        contractAddress: "0xc36442b4a4522e871399cd717abdd847ab11fe88",
-      },
-      {
-        chainId: "1",
-        contractAddress: "0x000000000022d473030f116ddee9f6b43ac78ba3",
-      },
-    ];
-
-    await Promise.all(
-      contracts.map(async ({ chainId, contractAddress }) => {
-        const projectPath = path.join(tempFolder, contractAddress);
-        const {
-          code,
-          contractName,
-          optimizationUsed,
-          runs,
-          viaIR,
-          solidityVersion,
-          rootContractPath,
-        } = await fetchContractInfo({ env: "local" }, chainId, contractAddress);
-
-        const { sources } = parseCode(code, contractName);
-
-        mkDirFromSources(cleanDirectories(sources), projectPath);
-
-        createHardhatConfig(
-          projectPath,
-          solidityVersion,
-          {
-            enabled: optimizationUsed,
-            runs,
-            viaIR,
-          },
-          true
-        );
-
-        const { execSync } = require("child_process");
-
-        const compileOutput = execSync("hardhat compile", {
-          cwd: projectPath,
-          encoding: "utf8",
-        });
-
-        expect(compileOutput).toMatch(
-          /Compiled \d+ Solidity files successfully/
-        );
-      })
-    );
+  afterAll(async () => {
+    // await fs.rm(tempFolder, { recursive: true, force: true });
   });
+
+  const testProjectStructure = async ({
+    chainId,
+    contractAddress,
+  }: Contract) => {
+    const projectPath = path.join(tempFolder, contractAddress);
+    const {
+      code,
+      contractName,
+      optimizationUsed,
+      runs,
+      viaIR,
+      solidityVersion,
+    } = await fetchContractInfo({ env: "local" }, chainId, contractAddress);
+
+    const { sources } = parseCode(code, contractName);
+
+    mkDirFromSources(cleanDirectories(sources), projectPath);
+
+    createHardhatConfig(
+      projectPath,
+      solidityVersion,
+      {
+        enabled: optimizationUsed,
+        runs,
+        viaIR,
+      },
+      true
+    );
+
+    const { execSync } = require("child_process");
+
+    const compileOutput = execSync("hardhat compile", {
+      cwd: projectPath,
+      encoding: "utf8",
+    });
+
+    expect(compileOutput).toMatch(
+      /Compiled \d+ Solidity file(s)? successfully/
+    );
+  };
+
+  it("works for single file contract", async () => {
+    const contract = {
+      chainId: "1",
+      contractAddress: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+    };
+
+    await testProjectStructure(contract);
+  }, 10000);
+
+  it("works with @ style libraries", async () => {
+    const contract = {
+      chainId: "1",
+      contractAddress: "0x29469395eaf6f95920e59f858042f0e28d98a20b",
+    };
+
+    await testProjectStructure(contract);
+  }, 10000);
+
+  it("works with remapped imports", async () => {
+    const contract = {
+      chainId: "1",
+      contractAddress: "0xe9bfe180c5696dd97db29791b1fa48ac833613ec",
+    };
+
+    await testProjectStructure(contract);
+  }, 10000);
 });
